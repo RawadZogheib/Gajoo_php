6ed60af44443d3d6c24842ddf35737ebd75a0cda
zogheibRawad <rawadzogheib@gmail.com>
2022-03-12 23:33:50 +0200

    .

---
+++ Jaas/index.php
@@ -0,0 +1,61 @@
+  <!DOCTYPE html>
+
+  <?php //instead of account it was user
+  if(!empty($_GET['userName']) && !empty($_GET['room'])){
+    $room = '"vpaas-magic-cookie-572bcefce781497ab789798973c88db0/'.htmlspecialchars($_GET['room']).'"';
+    $userName = htmlspecialchars("'".$_GET["userName"]."'");
+
+  }
+  else{
+    echo 'OOPs! Something went wrong. Try again in few seconds!!';
+  }
+  
+ ?>
+
+  <html>
+    <head>
+      <script src='https://8x8.vc/external_api.js' async></script>
+      <style>html, body, #jaas-container { height: 100%; }</style>
+      <script type="text/javascript">
+        let api;
+
+        const initIframeAPI = () => {
+          const domain = '8x8.vc';
+          const options = {
+            roomName: <?php echo $room;?>,
+            parentNode: document.querySelector('#jaas-container'),
+            configOverwrite: {
+              // disable the prejoin page
+              prejoinPageEnabled: false,
+              
+              //optionally we can control the mute state on join from the emebedding application
+              startWithAudioMuted: [true],
+              startWithVideoMuted: [true],
+              disableInitialGUM: true,
+              toolbarButtons: ['camera','chat','closedcaptions','desktop','download','embedmeeting','etherpad','filmstrip',
+                                'fullscreen','hangup','help','livestreaming','microphone','participants-pane','profile',
+                                'raisehand','recording','security','settings','shareaudio','sharedvideo','shortcuts','tileview',
+                                'toggle-camera','videoquality', '__end']
+            },
+              // optionally, we can have the meeting select the devices we want
+              devices: {
+                audioInput: '<deviceLabel>',
+                audioOutput: '<deviceLabel>',
+                videoInput: '<deviceLabel>'
+              },
+              interfaceConfigOverwrite: { SETTINGS_SECTIONS: [ 'devices', 'language', 'moderator', 'calendar', 'sounds' ] },
+              userInfo: {
+              displayName:<?php echo $userName?>
+              },
+          };
+          api = new JitsiMeetExternalAPI(domain, options);
+        }
+
+        window.onload = () => {
+            initIframeAPI();
+          }
+      
+      </script>
+    </head>
+    <body><div id="jaas-container" /></body>
+  </html>
