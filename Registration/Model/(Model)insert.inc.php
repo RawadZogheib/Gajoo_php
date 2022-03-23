749cea46f5b55e13fdf42bee30ff8c4a2aead796
Michel115 <91220948+Michel115@users.noreply.github.com>
2022-03-23 12:36:37 +0200

    .

--- Registration/Model/(Model)insert.inc.php
+++ Registration/Model/(Model)insert.inc.php
@@ -2,9 +2,9 @@
 
     $sql = "INSERT INTO `account`(`account_Id`, `account_firstName`, `account_lastName`, `account_userName`,
 	 `account_password`, `account_email`,
-    `account_phoneNumber`, `account_gender`, `account_date`, `isRegistered`)
+    `account_phoneNumber`, `account_gender`, `account_date`, `account_country`, `account_language`, `isRegistered`)
     VALUES (NULL,'".$fname."','".$lname."','".$userName."','".$hash."','".$email."','".$phoneNumberPlus."','".$gender."',
-    '".$dateOfBirth."', '".$isRegistered."')";
+    '".$dateOfBirth."', '".$country."', '".$language."', '".$isRegistered."')";
 
     $mq = mysqli_query($con,$sql);
 
