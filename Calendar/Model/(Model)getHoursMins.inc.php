b3118003e0f0dcb54923e40b6378ece3f7a0b67a
zogheibRawad <rawadzogheib@gmail.com>
2022-03-25 13:46:08 +0200

    Update (Model)getHoursMins.inc.php

--- Calendar/Model/(Model)getHoursMins.inc.php
+++ Calendar/Model/(Model)getHoursMins.inc.php
@@ -6,7 +6,7 @@
                                                 FROM `course_reserved` as cr
                                                 WHERE c.course_Id =cr.course_Id) as participants,
                     c.course_max_students,
-                    c.course_duration,
+                    c.course_duration
 
                 FROM `course` as c
                 WHERE c.course_date_of_begin BETWEEN '2022-3-27 00:00:00' AND '2022-3-27 23:59:59'";
