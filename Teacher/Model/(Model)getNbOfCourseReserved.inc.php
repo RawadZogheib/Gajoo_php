99b6c64da64b27d82a9ad0a59acfecbc7c447c15
Michel115 <91220948+Michel115@users.noreply.github.com>
2022-03-27 15:54:55 +0300

    Update (Model)getNbOfCourseReserved.inc.php

--- Teacher/Model/(Model)getNbOfCourseReserved.inc.php
+++ Teacher/Model/(Model)getNbOfCourseReserved.inc.php
@@ -1,7 +1,7 @@
 <?php
     $sql4 = "SELECT count(*)
 	FROM (SELECT count(*) as cnt, cr.course_Id as crcid FROM course_reserved as cr WHERE cr.course_Id IN (SELECT `course_Id` FROM course WHERE `teacher_Id` = '3') GROUP BY cr.course_Id) as counter, course  as c
-    WHERE crcid = c.course_Id AND cnt <= c.course_max_students ";
+    WHERE crcid = c.course_Id AND cnt = c.course_max_students ";
     $x4 = mysqli_query($con,$sql3);
     $res4 = mysqli_fetch_assoc($x4);
 
