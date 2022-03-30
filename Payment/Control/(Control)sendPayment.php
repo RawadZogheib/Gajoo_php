062fbea63a8693adf38b09a313419b55aece42c9
zogheibRawad <rawadzogheib@gmail.com>
2022-03-30 17:37:56 +0300

    .

--- Payment/Control/(Control)sendPayment.php
+++ Payment/Control/(Control)sendPayment.php
@@ -6,6 +6,9 @@ require $locVersionTest;
 $locTokenCheck = $_SERVER["DOCUMENT_ROOT"]  . '/gajoo_php/Config/Control/(Control)tokenCheck.php';
 if(require $locTokenCheck){
 
+    $locError7 =  $_SERVER["DOCUMENT_ROOT"]  . '/gajoo_php/Error/View/(View)error7.php';
+    if(!empty($data->teacher_Id)){
+
     $locGetcoupons = $_SERVER["DOCUMENT_ROOT"]  . '/gajoo_php/Payment/Model/(Model)getcoupons.inc.php';
     
     $json_array[0] = 'error4';
@@ -34,6 +37,8 @@ if(require $locTokenCheck){
     //echo '["'.$una.'","'.$maa.'","'.$mia.'"]';
         echo json_encode($json_array);
 
+        
+    }else require $locError7;
 }else require $_SERVER["DOCUMENT_ROOT"]  . '/gajoo_php/Error/View/(View)errorToken.php'; //JSON or GET is empty
  
 
