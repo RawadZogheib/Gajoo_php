9fab0e289893f3625c12dfc61e634177e0ee672d
RawadZogheib <rawadzogheib@gmail.com>
2022-06-26 11:32:12 +0300

    Update (Model)checkWalletsExists.inc.php

--- Calendar/Model/(Model)checkWalletsExists.inc.php
+++ Calendar/Model/(Model)checkWalletsExists.inc.php
@@ -1,5 +1,6 @@
 <?php
-    $sql2 = "SELECT (SELECT `coupon_amount` FROM `coupon` WHERE `account_Id` = '".$account_Id."' AND `payment_code` = '2') as coupon_amount2,
+    $sql2 = "SELECT (SELECT `coupon_amount` FROM `coupon` WHERE `account_Id` = '".$account_Id."' AND `payment_code` = '1') as coupon_amount1,
+                    (SELECT `coupon_amount` FROM `coupon` WHERE `account_Id` = '".$account_Id."' AND `payment_code` = '2') as coupon_amount2,
                     (SELECT `coupon_amount` FROM `coupon` WHERE `account_Id` = '".$account_Id."' AND `payment_code` = '3') as coupon_amount3,
                     (SELECT `coupon_amount` FROM `coupon` WHERE `account_Id` = '".$account_Id."' AND `payment_code` = '4') as coupon_amount4, 
                     (SELECT `coupon_amount` FROM `coupon` WHERE `account_Id` = '".$account_Id."' AND `payment_code` = '5') as coupon_amount5";
